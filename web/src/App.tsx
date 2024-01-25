faf531eb52e711ea38aac272c68f05df8f224eaf
“Xuqiuye <xuqiuye.li@gmail.com>
2024-01-26 00:51:10 +0900

    Add NoteListPage component to App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <>
-      <h1>MEMO</h1>
-    </>
-  )
-}
+import NetworkNoteRepository from './repository/NetworkNoteRepository.ts'
+import NoteListPage from "./component/NoteListPage.tsx";
 
-export default App
+export default function App() {
+    const noteRepo = new NetworkNoteRepository()
+    return (
+        <div>
+            <NoteListPage noteRepo={noteRepo} />
+        </div>
+    )
+}
